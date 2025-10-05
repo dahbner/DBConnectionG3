8a66faa8e7e0f957c6dca85b1fa3e6dc948d95f7
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-10-05 18:53:06 -0400

    feat: Creating GuestService Create Method using async functions

--- Services/GuestService.cs
+++ Services/GuestService.cs
@@ -1,6 +1,33 @@
-﻿namespace DBConnectionG3.Services
+﻿using DBConnectionG3.Models;
+using DBConnectionG3.Models.dtos;
+using DBConnectionG3.Repositories;
+
+namespace DBConnectionG3.Services
 {
-    public class GuestService
+    public class GuestService : IGuestService
     {
+        private readonly IGuestRepository _repo;
+
+        public GuestService(IGuestRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Guest> Create(CreateGuestDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto.FullName))
+            {
+                throw new InvalidOperationException("Full name is required");
+            }
+            var guest = new Guest
+            {
+                Id = Guid.NewGuid(),
+                FullName = dto.FullName.Trim(),
+                Confirmed = dto.Confirmed
+            };
+
+            await _repo.Add(guest);
+            return guest;
+        }
     }
 }
